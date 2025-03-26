0be6afabd2f956dca57511ebf5d66a7b1d2226bc
sam <majumdersamrat26@gmail.com>
2025-03-26 20:29:53 +0530

    aster

--- Bankist/script.js
+++ Bankist/script.js
@@ -29,3 +29,18 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// meaasge.textContent = 'We use cookied for improved functionality and analytics';
+message.innerHTML =
+  'We use cookied for improved functionality and analytics <button class="btn btn-close-cookie">Got it!</button>';
+
+header.append(message);
+
+document
+  .querySelector('.btn-close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
